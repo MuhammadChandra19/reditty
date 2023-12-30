f8efa60d6b541ee0772ca52a38e7586cf1224dfd
muhammad chandra zulfikar <muhammadchandrazulfikar@gmail.com>
2023-12-31 00:12:24 +0700

    feat: add rules

--- src/app/[[...slug]]/[components]/Listing.jsx
+++ src/app/[[...slug]]/[components]/Listing.jsx
@@ -1,6 +1,5 @@
 import { getListing } from '@/lib/service';
 import ListingCard from './ListingCard';
-import { LoadMore } from '@/components/shared/LoadMore';
 
 export default async function Listing({ pathName }) {
   const { data: result, after: param } = await getListing(pathName);
